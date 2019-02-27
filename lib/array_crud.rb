41a3c4d10d96280ee00b065cd25f1f56e17b509f
Denali Balser <denali.balser167@gmail.com>
2019-02-27 00:35:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -32,7 +32,7 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  array = ["am", 1, 2, 3, 4]
+  array = ["wow", 1, 2, 3, 4]
   array.first
 end
 
